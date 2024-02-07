d294a2bd5689471ad10083501d2740d040286e10
emraanadem <emraanadem@users.noreply.github.com>
2024-02-06 20:05:25 -0500

    2/6/24 8:05 PM
    Bug Fixes

--- Piston.py
+++ Piston.py
@@ -55,7 +55,7 @@ class Starters:
                     if val[2] == vals[1]:
                         Starters.evenmoreimpinfo.append([val[0],val[1],vals[0]])
                         with open(pathtwo + '/accinfo.json', 'w') as inst:
-                                inst.write(json.dumps(val[0],val[1]))
+                                inst.write(json.dump([val[0],val[1]], inst))
                         Starters.evenmoreimpinfo = []
         else:
             os.mkdir(pathtwo)
