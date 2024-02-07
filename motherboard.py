15fc98b5d22d192e55c810ac88a996ef2ec6009a
emraanadem <emraanadem@users.noreply.github.com>
2024-02-07 14:26:31 -0500

    2/7/24 2:26PM
    Final (hopefully) bug Fixes

--- motherboard.py
+++ motherboard.py
@@ -9,8 +9,8 @@ if __name__ == "__main__":
     #server = Server(Piston.borjan())
     #server.serve()
     threading.Thread(target=Pistontwo.borjan).start()
-    #threading.Thread(target=Piston.borjan).start()
-    #threading.Thread(target=PistonStocks.borjan).start()
+    threading.Thread(target=Piston.borjan).start()
+    threading.Thread(target=PistonStocks.borjan).start()
 
 
 # /* © 2022 Emraan Adem Ibrahim. See the license terms in the file 'license.txt' which should
--- swap_ends.py
+++ swap_ends.py
@@ -5,3 +5,17 @@ for val in Starters.accinfo:
                         with open(pathtwo + '/accinfo.json', 'w') as inst:
                                 inst.write(str(json.dump([val[0],val[1]], inst)))
                         Starters.evenmoreimpinfo = []
+
+
+
+                        with open('instrumentsthree.json', 'rb') as insts:
+            instrum = msgspec.json.decode(insts.read(), type=object)
+            info = instrum['instruments']
+            Starters.instrument_bank = info
+            for val in info:
+                placeholder = [val, rowid]
+                Starters.impinfo.append(placeholder)
+                rowid+=1
+                if rowid == 24:
+                    rowid = 0
+            
