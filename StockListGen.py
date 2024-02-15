375c4cc931822ab428c8319d3f29fe5c0854cffd
emraanadem <emraanadem@users.noreply.github.com>
2024-02-14 22:35:24 -0500

    2/14/24 10:35 PM
    Added StockList

---
+++ StockListGen.py
@@ -0,0 +1,9 @@
+import csv
+lists = []
+with open('nasdaqw.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',')
+    for row in spamreader:
+        if row[0] != 'Symbol':
+            lists.append(row[0])
+
+print(lists)
