a9d45f8e1aea8baab1ec63d0f34d2a6dc383fe75
emraanadem <emraanadem@users.noreply.github.com>
2024-02-17 12:14:58 -0500

    2/17/24 12:14 PM
    Bug Fixes

--- Analysis/ThirtyMin.js
+++ Analysis/ThirtyMin.js
@@ -11,6 +11,12 @@ const { createModel } = require('polynomial-regression');
 const nerdamer = require("nerdamer/all.min");
 const roots = require('kld-polynomial');
 
+const createClient = require('@supabase/supabase-js').createClient;
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient('https://nvlbmpghemfunkpnhwee.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im52bGJtcGdoZW1mdW5rcG5od2VlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDgxMTg3ODcsImV4cCI6MjAyMzY5NDc4N30.woZOGh5WaEcUtEyvsXaNP3Kg6BsNP8UOWhmv5RG4iMY')
+
+
 
 class Thirty_Min_Nexus{
 
